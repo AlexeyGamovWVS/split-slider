a85dcf8f9d0ecf957436256e547f2ddca1e24cc8
Alexey Gamov <lexa2009xxxx@yandex.ru>
2024-10-31 17:37:31 +0300

    publish

---
+++ readme.md
@@ -0,0 +1,53 @@
+# Сплит-слайдер для слайдов в 2 колонки (заготовка) v 1.0.0
+Проект кастомного слайдера с мнимальной адаптацией под другие экраны. Без использования библиотек и npm пакетов.
+
+HTML, Javascript, CSS
+
+Посмотреть: 
+
+## Использование:
+
+1. Скачать архив с шаблоном
+2. Скопировать в проект split-slider.js
+3. Импортировать класс в файл скрипта вашего приложения
+4. Создать объект данных для класса (см. пример в script.js)
+5. Скопировать styles.css в проект и доработать стили.
+
+Пример использования:
+
+```javascript
+import SplitSlider from './split-slider.js';
+
+const SPLIT_SLIDER_PARAMS = {
+  sliderSelector: '.slider',
+  sliderWrapperSelector: '.slider__wrapper',
+  slidesSelector: '.slide',
+  btnNextSelector: '.slider__next-btn',
+  btnPrevSelector: '.slider__prev-btn',
+  progressBarSelector: '.slider__progressbar',
+  progressBarStatusSelector: '.slider__progressstatus',
+  // animationDuration: 300, default
+  // nextSlideClass: "slide_next", default
+  // prevSlideClass: "slide_prev", default
+};
+
+const splitSlider = new SplitSlider(SPLIT_SLIDER_PARAMS);
+splitSlider.init();
+```
+
+## Пропсы для конструктора класса:
+
+- *sliderSelector* — селектор блока, в котором всё расположено
+- *sliderWrapperSelector* — селектор «карусели»
+- *slidesSelector* — общий селектор слайдов
+- *btnNextSelector*, *btnPrevSelector* — селекторы кнопок
+- *progressBarSelector*, *progressBarStatusSelector* — селекторы некликабельного прогресс-бара, отображающего текущий объём прокрутки.
+- *animationDuration* — длительность задержки перехода от одного слайда к другому, позволяющая отыграть транзишены из CSS, задаётся в миллисекундах, дефолтно - 300ms
+- *nextSlideClass* — класс слайда, следующего за активным, дефолтно *'slide_next'*,
+- *prevSlideClass* — класс слайда, идущего перед активным, дефолтно *'slide_prev'*,
+
+## Публичные методы класса SplitSlider:
+
+- *init()* — используется для запуска слайдера и слушателей
+- *rollToNextSlide()* – может быть назначен отдельно на свою кнопку, используется для перехода к следующему слайду
+- *rollToPrevSlide()* – может быть назначен отдельно на свою кнопку, используется для перехода к предыдущему слайду
---
+++ split-slider.js
@@ -0,0 +1,116 @@
+export default class SplitSlider {
+  constructor({
+    sliderSelector,
+    sliderWrapperSelector,
+    slidesSelector,
+    btnNextSelector,
+    btnPrevSelector,
+    progressBarSelector,
+    progressBarStatusSelector,
+    animationDuration = 300,
+    nextSlideClass = "slide_next",
+    prevSlideClass = "slide_prev",
+  }) {
+    this._slider = document.querySelector(sliderSelector);
+    this._sliderWrapper = this._slider.querySelector(sliderWrapperSelector);
+    this._slides = this._sliderWrapper.querySelectorAll(slidesSelector);
+    this._btnNext = this._slider.querySelector(btnNextSelector);
+    this._btnPrev = this._slider.querySelector(btnPrevSelector);
+    this._progressBar = this._slider.querySelector(progressBarSelector);
+    this._progressBarStatus = this._progressBar.querySelector(
+      progressBarStatusSelector
+    );
+    this._animationDuration = animationDuration;
+    this._nextSlideClass = nextSlideClass;
+    this._prevSlideClass = prevSlideClass;
+    this._currIndex = 0;
+    this._breakpoints = breakpoints;
+    this._amountSlides = this._slides.length;
+  }
+
+  init() {
+    this._resizeSlides();
+    this._restyleSlides();
+    this._addEventListeners();
+  }
+
+  rollToNextSlide = () => {
+    this._slides[this._currIndex].classList.add(this._prevSlideClass);
+
+    setTimeout(() => {
+      this._currIndex++;
+      if (this._currIndex >= this._amountSlides) {
+        this._currIndex = 0;
+      }
+      this._rollWrapper();
+      // Удаление .slide__next у нового текущего слайда, назначение его следующему
+      this._slides[this._currIndex].classList.remove(this._nextSlideClass);
+      const nextSlideIndex = (this._currIndex + 1) % this._amountSlides;
+      this._slides[nextSlideIndex].classList.add(this._nextSlideClass);
+    }, this._animationDuration);
+  };
+
+  rollToPrevSlide = () => {
+    this._slides[this._currIndex].classList.add(this._nextSlideClass);
+
+    setTimeout(() => {
+      this._currIndex--;
+      if (this._currIndex < 0) {
+        this._currIndex = this._amountSlides - 1;
+      }
+      this._rollWrapper();
+      // Удаление .slide__prev у нового текущего слайда, назначение его предыдущему
+      this._slides[this._currIndex].classList.remove(this._prevSlideClass);
+      const prevSlideIndex =
+        (this._currIndex - 1 + this._amountSlides) % this._amountSlides;
+      this._slides[prevSlideIndex].classList.add(this._prevSlideClass);
+    }, this._animationDuration);
+  };
+
+  _addEventListeners() {
+    window.addEventListener("resize", this._resizeSlides);
+    this._btnNext.addEventListener("click", this.rollToNextSlide);
+    this._btnPrev.addEventListener("click", this.rollToPrevSlide);
+  }
+
+  _rollWrapper() {
+    this._sliderWrapper.style.transform = `translateX(${
+      -this._currIndex * this._wrapperWidth
+    }px)`;
+    this._restyleSlides();
+    this._setProgressBarStatus();
+  }
+
+  _resizeSlides = () => {
+    this._wrapperWidth = this._slider.offsetWidth;
+    this._sliderWrapper.style.width = `${
+      this._wrapperWidth * this._amountSlides
+    }px`;
+    // this._slides.forEach((slide) => (slide.style.width = `${this._wrapperWidth}px`));
+    this._rollWrapper();
+  };
+
+  _restyleSlides() {
+    this._slides.forEach((slide, index) => {
+      slide.classList.remove(this._nextSlideClass, this._prevSlideClass);
+
+      if (index === (this._currIndex + 1) % this._amountSlides) {
+        slide.classList.add(this._nextSlideClass);
+      }
+
+      if (
+        index ===
+        (this._currIndex - 1 + this._amountSlides) % this._amountSlides
+      ) {
+        slide.classList.add(this._prevSlideClass);
+      }
+    });
+  }
+
+  _setProgressBarStatus = () => {
+    this._progressBarWidth = this._progressBar.offsetWidth;
+    this._progressBarStatus.style.width = `${
+      (this._progressBarWidth / this._amountSlides) * (this._currIndex + 1)
+    }px`;
+  };
+}
